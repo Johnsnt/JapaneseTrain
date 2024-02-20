66ce5e1edd87a8557b98a6927fe4c8c549d0d703
Johnny Santos <149924300+Johnsnt@users.noreply.github.com>
2024-02-20 18:46:34 -0300

    Arquivos organizado em componentes com a herança de state funcionais

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,12 @@
 import "./Card.css";
 
-export default function GalleryItem(props) {
+export default function GalleryItem({name, img}) {
     return (
     <div className="container-image">
             <div className="container-titulo">
-            <h1>{props.name}</h1>
+            <h1>{name}</h1>
             </div>
-            <img src={props.img} alt={props.name} className="imagem" />
+            <img src={img} alt={name} className="imagem" />
         </div>
     )
 };
--- src/components/TrainList/index.jsx
+++ src/components/TrainList/index.jsx
@@ -1,42 +1,13 @@
 import './TrainList.css';
-import { useState } from 'react';
 import GalleryItem from '../Card';
-import {arrow, eastJRLogo} from '../../assets/items/'
-import {returnTrains, filterTrain} from '../../servico/'
+import { arrow } from '../../assets/items/';
 
-export default function TrainList() {
-    //Estado para armazernar o tipo de trem
-    const [trainType, setTrainType] = useState(returnTrains());
-
-    //Atualiza o tipo de trem ao pressionar o botão
-    const handleTrainTypeFilter = (trainType) => {
-        setTrainType(filterTrain(trainType));
-    };
-
-    //Limpa o filtro
-    const handleClearFilter = () => {
-        setTrainType(returnTrains());
-    };
-
+export default function TrainList({ trainCollection }) {
     return (
         <>
-            <header>
-            <img src={eastJRLogo} alt="JR_Logo" id="logo" />
-            <div className="btn-filter">
-            <button onClick={() => handleTrainTypeFilter("express")}>Special Express</button>
-            <button onClick={() => handleTrainTypeFilter("commuter")}>Commuter Rail</button>
-            <button onClick={handleClearFilter}>Limpar Filtro</button>
-            </div>
-            </header>
-
-            <a href="#top" className="arrowBtn">
-                <img src={arrow} alt="topArrow" />
-                
-            </a>
-            
-            <section className="secao-train-images" id="top">
+            <section className="train-image-section" id="top">
                 <div className="gallery">
-                    {trainType.map((item) => (
+                    {trainCollection.map((item) => (
                         <GalleryItem
                             key={item.id}
                             name={item.name}
@@ -44,10 +15,10 @@ export default function TrainList() {
                         />
                     ))}
                 </div>
+                <a href="#top" className="arrowBtn">
+                    <img src={arrow} alt="topArrow" />
+                </a>
             </section>
-            <footer>
-                <p>aaaa awdwfa</p>
-            </footer>
         </>
     );
 };
